87b5f76cf57f3ebbe5bd68802b1d03afabea7f96
danielaparker <danielaparker@yahoo.com>
2018-01-21 19:00:02 -0500

    integer range test case

--- tests/src/jcr/jcr_parser_tests.cpp
+++ tests/src/jcr/jcr_parser_tests.cpp
@@ -57,6 +57,24 @@ BOOST_AUTO_TEST_CASE(jcr_test2)
     BOOST_CHECK(!ec);
 }
 
+BOOST_AUTO_TEST_CASE(jcr_test3)
+{
+    std::string s = R"(
+{ "line-count" : 0.. , "word-count" : 0.. }
+    )";
+
+    null_jcr_input_handler handler;
+    jcr_parser parser(s.data(),s.length(),handler);
+
+    std::error_code ec;
+    parser.parse(ec);
+    if (ec)
+    {
+        std::cerr << ec.message() << std::endl;
+    }
+    BOOST_CHECK(!ec);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 
